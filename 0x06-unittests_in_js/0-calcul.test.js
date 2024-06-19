28d4955a87f042ab352eaaa9b470bea9bab5f3f0
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-19 22:16:36 +0200

    Updated 0-calcul.test.js - Added additional tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,24 +9,45 @@ describe('calculateNumber test', function() {
 			assert.equal(expected, 3);
 		});
 	});
-	// only a needs to be rounded
+	// only a needs to be rounded (floor)
 	describe('a to be rounded', function() {
 		it('expecting 2', function() {
 			const expected = calculateNumber(1.4, 1);
 			assert.equal(expected, 2);
 		});
 	});
-	// only b needs to be rounded
-	describe('b to be rounded', function() {
+	// only a needs to be rounded (ceil)
+	describe('a to be rounded', function() {
+		it('expecting 3', function() {
+			const expected = calculateNumber(1.8, 1);
+			assert.equal(expected, 3);
+		});
+	});
+	// only b needs to be rounded (ceil)
+	describe('b to be rounded (ceil)', function() {
 		it('expecting 4', function() {
 			const expected = calculateNumber(1, 2.7);
 			assert.equal(expected, 4);
 		});
 	});
-	// both a and b need to be rounded
-	describe('a and b to be rounded', function() {
+	// only b needs to be rounded (floor)
+	describe('b to be rounded (floor)', function() {
+		it('expecting 3', function() {
+			const expected = calculateNumber(1, 2.4);
+			assert.equal(expected, 3);
+		});
+	});
+	// both a and b need to be rounded (ceil)
+	describe('a and b to be rounded (ceil)', function() {
 		it('expecting 10', function() {
 			const expected = calculateNumber(1.5, 7.9);
+			assert.equal(expected, 10);
+		});
+	});
+	// both a and b need to be rounded (floor)
+	describe('a and b to be rounded (floor)', function() {
+		it('expecting 10', function() {
+			const expected = calculateNumber(2.1, 8.4);
 			assert.equal(expected, 10);
 		});
 	});
