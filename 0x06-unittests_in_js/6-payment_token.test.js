1bdc2deb586938d9df9383d43308369f38a20208
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-06-20 18:55:26 +0200

    Updated 6-payment_token.test.js - Added another test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,21 @@ const expect = require('chai').expect
 const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
 describe('getPaymentTokenFromAPI', function () {
-	it('should always resolve', function () {
+	// When true is passed
+	it('should resolve', function () {
 		const expectedData = {data: 'Successful response from the API' };
 		getPaymentTokenFromAPI(true)
 		.then((data) => {
 			expect(data).to.equal(expectedData);
+			done();
+		})
+	});
+	// When false is passed
+	it('should do nothing', function () {
+		const expectedData = {data: 'Successful response from the API' };
+		getPaymentTokenFromAPI(false)
+		.then((data) => {
+			expect(data).to.equal(undefined);
 			done();
 		})
 	});
